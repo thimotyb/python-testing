fd90fdf68258d3da5e70d30a081ff2d1e2caed2f
Thimoty Barbieri <thimoty@thimoty.it>
2023-11-20 23:42:38 +0100

    refactoring

---
+++ opentelemetry/02-server_with_jaeger.py
@@ -0,0 +1,39 @@
+# pip install opentelemetry-exporter-jaeger
+# docker run -d --name jaeger -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 -p 5775:5775/udp -p 6831:6831/udp -p 6832:6832/udp -p 5778:5778 -p 16686:16686 -p 14268:14268 -p 14250:14250 -p 9411:9411 jaegertracing/all-in-one:1.23
+# Jaeger host: http://localhost:16686/
+from flask import Flask
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(
+  TracerProvider(
+    resource=Resource.create({SERVICE_NAME: "my-python-service"})
+  )
+)
+jaeger_exporter = JaegerExporter(
+  agent_host_name="localhost",
+  agent_port=6831,
+)
+trace.get_tracer_provider().add_span_processor(
+  BatchSpanProcessor(jaeger_exporter)
+)
+
+name = "test"
+tracer = trace.get_tracer(name)
+
+app = Flask(name)
+ 
+@app.route('/')
+def index():
+  with tracer.start_as_current_span("server_request"):
+    return 'Hello World!'
+ 
+app.run(host='0.0.0.0', port=8000)

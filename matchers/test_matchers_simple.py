4a710ef936223f85c2c2d8fe7aabb3483e015169
Thimoty Barbieri <thimoty@thimoty.it>
2023-11-22 21:56:23 +0100

    added matcher

---
+++ matchers/test_matchers_simple.py
@@ -0,0 +1,25 @@
+# pip install PyHamcrest
+# python -m unittest test_matchers_simple.py
+import sys
+
+sys.path.append("..")
+
+from hamcrest import *
+import unittest
+
+
+class ExampleWithAssertThat(unittest.TestCase):
+    def testUsingAssertThat(self):
+        assert_that("xx", is_("xx"))
+        assert_that("yy", is_not("xx"))
+        assert_that("i like cheese", contains_string("cheese"))
+
+    def testCanAlsoSupplyDescriptiveReason(self):
+        assert_that("xx", is_("xx"), "description")
+
+    def testCanAlsoAssertPlainBooleans(self):
+        assert_that(True, "This had better not fail")
+
+
+if __name__ == "__main__":
+    unittest.main()
